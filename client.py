fcfb46ad57365b596d367b032e9e1dd9110e66ec
Ubuntu <ubuntu@ip-172-31-16-46.us-east-2.compute.internal>
2019-06-04 04:13:54 +0000

    added client.py file

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    try:
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sd:
+            sd.connect(('<put here dns>',9990))
+            msg = input('Enter message : ')
+            if len(msg.strip()) > 0:
+                logger.info('You will send <<{}>> to server'.format(msg))
+                sd.sendall(msg.encode())
+                response = sd.recv(1024)
+            else:
+                logger.info('You must enter a message!!! Try again')
+                sys.exit(1)
+
+        print(response.decode())
+    except Exception as e:
+        logger.info(e)
+
